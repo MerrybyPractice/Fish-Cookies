c0e530ebd240fe00a35973fd4a97fa7fe4be2418
Mary Cimakasky <MaryCimakasky@Marys-MacBook-Pro.local>
2019-02-11 21:39:08 -0800

    got the lists appearing correctly on the page

--- app.js
+++ app.js
@@ -8,25 +8,17 @@ As a developer, I want to use object-oriented programming to build this site, so
 
 Technical Requirements
 
-
-Good use of Object Literals (no constructors allowed today); one for each store model; properties/values and methods are correctly constructed and given meaningful names
 Main page meets requirements of the problem domain
 Use template literals in your JS logic to render the stores as lists on the sales page
 
 User Stories (Stretch... only after completing everything above)
 As a developer, I want to make some headway on the public-facing page for the business
 
-
-*/
-
-/* obj literals for each cookie stand
+    obj literals for each cookie stand
     each needs to:
 
-
-      calculate and store the simulated ammounts of cookies purchased for each hour location using average cookies purchased and the random number of cookies generated
-      store the results for each location in a seperate array...perhaps as a property of the object representing that location
       display the values of each array as an unordered list in the browser
-      display the sum of hourly tottlas in this format
+      display the sum of hourly tottals in this format
         location
           time: number cookies
 
@@ -40,26 +32,23 @@ As a developer, I want to make some headway on the public-facing page for the bu
 //variable max num cust per hour
 //variable num cookies purchased per cust
 
-//time - write in military time. When I print it, check if 12 has happened. If === 12, switch to pm, if 12 has appeared, append 12pm and subtract 12.
+
 
 // cookie counter function
 
 function cookie_counter(store) {
 
   for(var i = 6; i<20; i++){
-    var CPH = Math.floor(store.CPH()*store.avg_cookie);
-    store.customer_array.push(CPH);
+    var cph = Math.floor(store.cph()*store.avg_cookie);
+    store.customer_array.push(cph);
 
-    console.log(CPH);
+    console.log(cph);
   }
   console.log(store.customer_array);
 }
 
-
 var all_hours = '14';
 console.log('hello world!!!');
-
-
 
 //1st and Pike
 
@@ -69,19 +58,14 @@ var first_and_pike = {
   max_cust: '65',
   avg_cookie: '6.3',
   customer_array: [ ],
-  CPH: function() {
+  cph: function() {
     var min = Math.ceil(this.min_cust);
     var max = Math.floor(this.max_cust);
     return Math.floor(Math.random()*(max - min +1))+min;
   },
 };
 
-
-
-
 console.log('first and pike array '+cookie_counter(first_and_pike));
-
-
 
 //SeaTac AirPort
 
@@ -91,7 +75,7 @@ var seatac = {
   max_cust: '24',
   avg_cookie: '1.2',
   customer_array: [ ],
-  CPH: function () {
+  cph: function () {
     var min = Math.ceil(this.min_cust);
     var max = Math.floor(this.max_cust);
     return Math.floor(Math.random()*(max-min +1))+min;
@@ -109,7 +93,7 @@ var sea_cen = {
   max_cust: '38',
   avg_cookie: '3.7',
   customer_array: [],
-  CPH: function (){
+  cph: function (){
     var min = Math.ceil(this.min_cust);
     var max = Math.floor(this.max_cust);
     return Math.floor(Math.random()*(max-min))+min;
@@ -125,7 +109,8 @@ var cap_hill = {
   min_cust: '20',
   max_cust: '38',
   avg_cookie: '2.3',
-  CPH: function (){
+  customer_array: [],
+  cph: function (){
     var min = Math.ceil(this.min_cust);
     var max = Math.floor(this.max_cust);
     return Math.floor(Math.random()*(max-min))+min;
@@ -141,10 +126,39 @@ var alki = {
   min_cust: '2',
   max_cust: '16',
   avg_cookie: '4.6',
-  alki_CPH: function() {
+  customer_array: [],
+  cph: function() {
     var min = Math.ceil(this.min_cust);
     var max = Math.floor(this.max_cust);
     return Math.floor(Math.random()*(max-min))+min;
   }
 };
 console.log(cookie_counter(alki));
+
+
+var page_div = document.createElement('div');
+page_div.textContent = 'Sales Numbers';
+document.body.appendChild(page_div);
+var ul = document.createElement('ul');
+var li = document.createElement('li');
+
+document.body.appendChild(ul);
+document.getElementsByTagName('ul')[0].appendChild(li);
+function store_list(store_array){
+  for(var t=6; t < 21; t++){
+
+    var hour=t+'am';
+    if (t >= 12 ){
+      hour = t + 'pm';
+    }
+    if (t > 12 ){
+      hour = t-12+'pm';
+    }
+    console.log(hour);
+    li.textContent =`${hour}+:${store_array[t]}Cookies`;
+  }
+ 
+  
+}
+console.log(store_list (first_and_pike.customer_array));
+console.log('dfgh');
