2b111f60223589345f282a81db6a740b83505553
Mary Cimakasky <MaryCimakasky@Marys-MacBook-Pro.local>
2019-02-12 18:52:42 -0800

    got the time function corrected

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@
 
 User Stories (MVP)
 Priotity 2: A table to show the total amount of projected cookie needs at each location, with the table displaying the cookie stand location, the total number of cookies needed for each location, an hourly breakdown of total cookies sales for each location, and a footer row of totals for each column.
-Priority 1: As a developer, I want to implement a constructor function, so that I can reuse code and eliminate much of the duplication in my JavaScript
+
 Technical Requirements (MVP)
-Good use of a constructor function; style and syntax are correctly implemented
 Each cookie stand location should have a separate render() method that creates and appends its row to the table
 The header row and footer row are each created in their own stand-alone function
 Duplicate code has been removed and DRY principles are evident
@@ -14,26 +13,73 @@ Working on a non-master branch for the day, with regular commit history. Basical
 */
 
 
-/*
-// helper funcitons
-//var _random = function(min, max){
 
-//}
+// helper funcitons
+
+//this converts the time measure from 24 hour clock to 12 hour clock.
+function print_time_conversion (store){
+
+  for(var t= store.store_open; t< store.store_closed;t++){
+
+    if (t<12){
+      var hour = t + 'am';
+
+    }
+
+    else if (t === 12){
+      // eslint-disable-next-line no-redeclare
+      var hour = t + 'pm';
+    }
+
+    else {
+      // eslint-disable-next-line no-redeclare
+      var hour = t-12+'pm';
+    }
+
+    var tr = document.createElement('tr');
+    tr.textContent = `${hour}:`;
+    document.getElementById(store.store_location).appendChild(tr);
+    if(t===20){
+      break;
+    }
+
+
+  }
+}
+
+function print_number_cookies (store){
+  for(var t= store.store_open; t< store.store_closed;t++){
+    var tr = document.createElement('tr');
+    tr.textContent = `${store.customer_array} Cookies`;
+    document.getElementById(store.store_location).appendChild(tr);
+    if (t===20){
+      break;
+    }
+  }
+}
 // constructor functions
 
 var Store = function(store_location, store_open, store_closed, min_cust, max_cust, avg_cookie){
-this.store_location = store_location;
-this.store_open = store_open;
-this.store_closed = store_closed;
-this.min_cust = min_cust;
-this.max_cust = max_cust;
-this.avg_cookie = avg_cookie;
-
-
-// }
+  this.store_location = store_location;
+  this.store_open = store_open;
+  this.store_closed = store_closed;
+  this.duration = store_closed - store_open;
+  this.min_cust = min_cust;
+  this.max_cust = max_cust;
+  this.avg_cookie = avg_cookie;
+  this.customer_array = [ ];
+  this.cph = function (){
+    for (var i = 0; i < this.duration; i++){
+      var min = Math.ceil(this.min_cust);
+      var max = Math.floor (this.max_cust);
+      var num = Math.floor(Math.random()*(max-min+1))+min;
+      this.customer_array.push(num);
+    }
+  };
+};
 
 // ============================
-*/
+
 
 
 // cookie counter function
@@ -50,92 +96,34 @@ this.avg_cookie = avg_cookie;
 // function cookie_sum(store){
 // }
 
+
+
 //1st and Pike
+var first_and_pike = new Store('First and Pike', 6, 20, 23, 65, 6.3) ;
 
-var first_and_pike = {
-  store_location: 'First and Pike',
-  store_open: 6,
-  store_closed: 20,
-  min_cust: '23',
-  max_cust: '65',
-  avg_cookie: '6.3',
-  customer_array: [ ],
-  cph: function() {
-    var min = Math.ceil(this.min_cust);
-    var max = Math.floor(this.max_cust);
-    return Math.floor(Math.random()*(max - min +1))+min;
+console.log(first_and_pike);
 
-  },
-  //cah:
-};
 //SeaTac AirPort
 
-var seatac = {
-  store_location: 'SeaTac Airport',
-  store_open: 6,
-  store_closed: 20,
-  min_cust: '3',
-  max_cust: '24',
-  avg_cookie: '1.2',
-  customer_array: [ ],
-  cph: function () {
-    var min = Math.ceil(this.min_cust);
-    var max = Math.floor(this.max_cust);
-    return Math.floor(Math.random()*(max-min +1))+min;
-  }
-  //cah:
-};
+var seatac = new Store('SeaTac Airport', 6, 20, 3, 24, 1.2);
+
+console.log(seatac);
+
 //Seattle Center
 
-var sea_cen = {
-  store_location: 'Seattle Center',
-  store_open: 6,
-  store_closed: 20,
-  min_cust: '11',
-  max_cust: '38',
-  avg_cookie: '3.7',
-  customer_array: [],
-  cph: function (){
-    var min = Math.ceil(this.min_cust);
-    var max = Math.floor(this.max_cust);
-    return Math.floor(Math.random()*(max-min))+min;
-  }
-  //cah:
-};
+var sea_cen = new Store ('Seattle Center', 6, 20, 11, 38, 3.7);
+
+console.log(sea_cen);
+
 //Capitol Hill
 
-var cap_hill = {
-  store_location: 'Capitol Hill',
-  store_open: 6,
-  store_closed: 20,
-  min_cust: '20',
-  max_cust: '38',
-  avg_cookie: '2.3',
-  customer_array: [],
-  cph: function (){
-    var min = Math.ceil(this.min_cust);
-    var max = Math.floor(this.max_cust);
-    return Math.floor(Math.random()*(max-min))+min;
-  }
-  //cah;
-};
+var cap_hill = new Store ('Capitol Hill', 6, 20, 20, 38, 2.3);
+
+console.log(cap_hill);
+
 //Alki
 
-var alki = {
-  store_location: 'Alki',
-  store_open: 6,
-  store_closed: 20,
-  min_cust: '2',
-  max_cust: '16',
-  avg_cookie: '4.6',
-  customer_array: [],
-  cph: function() {
-    var min = Math.ceil(this.min_cust);
-    var max = Math.floor(this.max_cust);
-    return Math.floor(Math.random()*(max-min))+min;
-  }
-  //cah;
-};
+var alki = new Store ('Alki', 6, 20, 2, 16, 4.6);
 
 var page_div = document.createElement('div');
 page_div.textContent = 'Sales Numbers';
@@ -145,36 +133,35 @@ function store_list(store){
   var h4 = document.createElement('h4');
   h4.textContent = `${store.store_location}`;
   document.getElementsByTagName('div')[0].appendChild(h4);
-  var ul = document.createElement('ul');
-  ul.setAttribute('id', store.store_location);
-  document.getElementsByTagName('div')[0].appendChild(ul);
-  //this iterates through the hours each shop is open in 24h time and lists it in standard american AM/PM.
-  for(var t=store.store_open; t < store.store_closed; t++){
+  var table = document.createElement('table');
+  table.setAttribute('id', store.store_location);
+  document.getElementsByTagName('div')[0].appendChild(table);
 
-    var hour=t+'am';
-    if (t >= 12 ){
-      hour = t + 'pm';
-    }
-    if (t > 12 ){
-      hour = t-12+'pm';
-    }
+  print_time_conversion(store);
+  print_number_cookies(store);
 
-    var li = document.createElement('li');
-    li.textContent =`${hour} : ${store.customer_array[t-6]} Cookies`;
-    document.getElementById(store.store_location).appendChild(li);
-    if(t===20){
-      break;
-    }
-    // var li_two = document.createElement('li');
-    // li_two.textContent=`Tottal: ${cookie_sum(store)} Cookies`,
-    // document.getElementById(store.store_list).appendChild(li_two);
-  }
+  // var tr = document.createElement('tr');
+  // tr.textContent =`: ${store.customer_array[t-6]} Cookies`;
+  // document.getElementById(store.store_location).appendChild(tr);
+
+  // if(t===20){
+  //   break;
+  // }
+  // var tr_two = document.createElement('tr');
+  // tr_two.textContent=`Tottal: ${cookie_sum(store)} Cookies`,
+  // document.getElementById(store.store_list).appendChild(tr_two);
+
 }
 
+first_and_pike.cph();
 store_list(first_and_pike);
+seatac.cph();
 store_list(seatac);
+sea_cen.cph();
 store_list(sea_cen);
+cap_hill.cph();
 store_list(cap_hill);
+alki.cph();
 store_list(alki);
 
 
